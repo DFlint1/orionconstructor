78bd771b712b85e95608e04ba5dab19b7d05821b
Dana Flint <Dana_Flint@msn.com>
2017-04-28 05:30:19 -0500

    changed back

---
+++ test.js
@@ -0,0 +1,64 @@
+// MongoDB Configuration configuration
+// mongoose.connect('mongodb://heroku_g7g209gf:c0ipjvd8t73p0aunil2sa59c8f@ds027749.mlab.com:27749/heroku_g7g209gf');
+//     //"mongodb://localhost/projects");
+//     var db=process.env.MONDB_URI || "mongodb://localhost/projects";
+// var db = mongoose.connection;
+// {
+//   "name": "orionconstructor",
+//   "version": "1.0.0",
+//   "description": "",
+//   "main": "server.js",
+//   "dependencies": {
+//     "babel-cli": "^6.24.1",
+//     "babel-core": "^6.24.1",
+//     "babel-plugin-add-module-exports": "^0.2.1",
+//     "babel-plugin-react-html-attrs": "^2.0.0",
+//     "babel-preset-latest": "^6.24.1",
+//     "babel-preset-node6": "^11.0.0",
+//     "babel-preset-stage-0": "^6.24.1",
+//     "body-parser": "^1.15.1",
+//     "bootstrap": "^3.3.7",
+//     "express": "^4.13.4",
+//     "history": "^1.17.0",
+//     "moment": "^2.18.1",
+//     "mongoose": "^4.4.20",
+//     "morgan": "^1.7.0",
+//     "react": "^15.1.0",
+//     "react-bootstrap": "^0.30.10",
+//     "react-dom": "^15.1.0",
+//     "react-router": "^2.4.1",
+//     "request": "^2.81.0",
+//     "request-promise": "^4.2.0"
+//   },
+//   "devDependencies": {
+//     "axios": "^0.12.0",
+//     "babel-core": "^6.9.1",
+//     "babel-loader": "^6.2.4",
+//     "babel-plugin-transform-class-properties": "^6.24.1",
+//     "babel-plugin-transform-decorators-legacy": "^1.3.4",
+//     "babel-preset-es2015": "^6.24.1",
+//     "babel-preset-react": "^6.5.0",
+//     "css-loader": "^0.23.1",
+//     "jquery": "^3.1.1",
+//     "react": "^15.3.2",
+//     "react-addons-test-utils": "^15.3.0",
+//     "react-dom": "^15.3.2",
+//     "react-scripts": "0.7.0",
+//     "style-loader": "^0.13.1",
+//     "uuid": "^2.0.3",
+//     "webpack": "^1.13.1",
+//     "webpack-dev-server": "^1.14.1"
+//   },
+//   "engines":{
+//     "node":"7.7.1"
+//   },
+//   "scripts": {
+//     "dev": "react-scripts start",
+//     "build": "react-scripts build",
+//     "test": "react-scripts test --env=jsdom",
+//     "eject": "react-scripts eject",
+//     "start":"node server.js"
+//   },
+//   "author": "",
+//   "license": "ISC"
+// }
